da736192fd34e205b68fcce548e1ba5b79180986
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2025-08-08 20:24:00 +0500

    Added the customer report datamart DDL

---
+++ scripts/DDL_customer_report_datamart.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS dwh.customer_report_datamart;
+
+CREATE TABLE IF NOT EXISTS dwh.customer_report_datamart (
+    id BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    customer_id BIGINT NOT NULL,
+    customer_name VARCHAR NOT NULL,
+    customer_address VARCHAR NOT NULL,
+    customer_birthday DATE NOT NULL,
+    customer_email VARCHAR NOT NULL,
+    customer_spend NUMERIC(15,2) NOT NULL,
+    platform_money BIGINT NOT NULL,
+    count_order BIGINT NOT NULL,
+    avg_price_order NUMERIC(10,2) NOT NULL,
+    median_time_order_completed NUMERIC(10,1),
+    top_product_category VARCHAR NOT NULL,
+    top_craftsman_id BIGINT NOT NULL,
+    count_order_created BIGINT NOT NULL,
+    count_order_in_progress BIGINT NOT NULL,
+    count_order_delivery BIGINT NOT NULL,
+    count_order_done BIGINT NOT NULL,
+    count_order_not_done BIGINT NOT NULL,
+    report_period VARCHAR NOT NULL,
+    CONSTRAINT customer_report_datamart_pk PRIMARY KEY (id)
+    -- add FK for top_craftsman_id to the d_craftsman table?
+);
