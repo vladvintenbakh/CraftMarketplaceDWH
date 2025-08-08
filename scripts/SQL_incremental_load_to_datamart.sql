de94e4de84016d3b7e678390b2d176d39082290e
Vlad Vintenbakh <v.vintenbakh@gmail.com>
2025-08-08 20:52:03 +0500

    Added the dwh_update_delta CTE

--- scripts/SQL_incremental_load_to_datamart.sql
+++ scripts/SQL_incremental_load_to_datamart.sql
@@ -16,15 +16,21 @@ dwh_delta AS (
 	    dc.load_dttm AS craftsman_load_dttm,
 	    dcs.load_dttm AS customers_load_dttm,
 	    dp.load_dttm AS products_load_dttm
-	    FROM dwh.f_order fo 
-	    INNER JOIN dwh.d_craftsman dc ON fo.craftsman_id = dc.craftsman_id 
-	    INNER JOIN dwh.d_customer dcs ON fo.customer_id = dcs.customer_id 
-	    INNER JOIN dwh.d_product dp ON fo.product_id = dp.product_id 
-	    LEFT JOIN dwh.customer_report_datamart crd ON dcs.customer_id = crd.customer_id
-	    WHERE 
-	    	(fo.load_dttm > (SELECT COALESCE(MAX(load_dttm),'1900-01-01') FROM dwh.load_dates_customer_report_datamart)) OR
-	    	(dc.load_dttm > (SELECT COALESCE(MAX(load_dttm),'1900-01-01') FROM dwh.load_dates_customer_report_datamart)) OR
-	        (dcs.load_dttm > (SELECT COALESCE(MAX(load_dttm),'1900-01-01') FROM dwh.load_dates_customer_report_datamart)) OR
-	        (dp.load_dttm > (SELECT COALESCE(MAX(load_dttm),'1900-01-01') FROM dwh.load_dates_customer_report_datamart))
+    FROM dwh.f_order fo 
+    INNER JOIN dwh.d_craftsman dc ON fo.craftsman_id = dc.craftsman_id 
+    INNER JOIN dwh.d_customer dcs ON fo.customer_id = dcs.customer_id 
+    INNER JOIN dwh.d_product dp ON fo.product_id = dp.product_id 
+    LEFT JOIN dwh.customer_report_datamart crd ON dcs.customer_id = crd.customer_id
+    WHERE 
+        (fo.load_dttm > (SELECT COALESCE(MAX(load_dttm),'1900-01-01') FROM dwh.load_dates_customer_report_datamart)) OR
+        (dc.load_dttm > (SELECT COALESCE(MAX(load_dttm),'1900-01-01') FROM dwh.load_dates_customer_report_datamart)) OR
+        (dcs.load_dttm > (SELECT COALESCE(MAX(load_dttm),'1900-01-01') FROM dwh.load_dates_customer_report_datamart)) OR
+        (dp.load_dttm > (SELECT COALESCE(MAX(load_dttm),'1900-01-01') FROM dwh.load_dates_customer_report_datamart))
+),
+dwh_update_delta AS (
+    SELECT     
+    	dd.exist_customer_id AS customer_id
+    FROM dwh_delta dd 
+    WHERE dd.exist_customer_id IS NOT NULL        
 )
 SELECT 'increment datamart';
